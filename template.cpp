d73f27afa01e0b3003982a3f146e7314305e1b9d
Pavel Zeman <pavel@pavel>
2014-05-31 23:14:35 +0200

    Added getInt to the template

--- template.cpp
+++ template.cpp
@@ -9,6 +9,18 @@
 #define FOR(c, m) for(int c=0;c<(m);c++)
 #define FORE(c, f, t) for(int c=(f);c<(t);c++)
 
+char c = 0;
+int getInt() {
+    int r = 0;
+    while (!(c == '-' || (c >= '0' && c <= '9'))) c = getc_unlocked(stdin);
+    bool minus = c == '-';
+    if (minus) c = getc_unlocked(stdin);
+    while (c >= '0' && c <= '9') {
+        r = r * 10 + (c - '0');
+        c = getc_unlocked(stdin);
+    }
+    return minus ? -r : r;
+}
 
 int main(void) {
     int cases;
