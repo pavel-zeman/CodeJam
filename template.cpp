47f2d5f5e8225fe6c3bdc71c133679ce3c624ba8
Pavel Zeman <pavel@pavel.cz>
2016-04-11 23:00:00 +0200

    Updated template

--- template.cpp
+++ template.cpp
@@ -1,31 +1,39 @@
-#include <stdio.h>
-#include <string.h>
-#include <limits.h>
-#include <math.h>
-#include <stdlib.h>
-#include <ctype.h>
-#include <assert.h>
+// Description
+// URL
 
-#define FOR(c, m) for(int c=0;c<(m);c++)
+#include <bits/stdc++.h>
+using namespace std;
+
+#define FOR(c, m) for(int c=0;c<(int)(m);c++)
 #define FORE(c, f, t) for(int c=(f);c<(t);c++)
+#define LLI long long int
+#define MOD 1000000007
 
-char c = 0;
-int getInt() {
-    int r = 0;
-    while (!(c == '-' || (c >= '0' && c <= '9'))) c = getc_unlocked(stdin);
-    bool minus = c == '-';
-    if (minus) c = getc_unlocked(stdin);
-    while (c >= '0' && c <= '9') {
-        r = r * 10 + (c - '0');
-        c = getc_unlocked(stdin);
+int power(int a, int b) {
+    int sq = a;
+    int result = 1;
+    while (b > 0) {
+        if ((b & 1) == 1) result = result * (LLI)sq % MOD;
+        sq = sq * (LLI)sq % MOD;
+        b >>= 1;
     }
-    return minus ? -r : r;
+    return result;
 }
+
+int inv(int a) {
+    return power(a, MOD - 2);
+}
+
+void fail() {
+    *((char *)0) = 0;
+}
+
+
 
 int main(void) {
     int cases;
     scanf("%d", &cases);
-    for(int c=1;c<=cases;c++) {
-        printf("Case #%d:", c);
+    for(int cc=1;cc<=cases;cc++) {
+        printf("Case #%d:", cc);
     }
 }
