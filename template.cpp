8ad76a986c56c124980972b3bc51846a63e90b41
Pavel Zeman <pavel@pavel.cz>
2018-04-09 14:51:58 +0200

    Modified template for 2018

--- template.cpp
+++ template.cpp
@@ -1,13 +1,83 @@
 // Description
 // URL
-
 #include <bits/stdc++.h>
 using namespace std;
 
 #define FOR(c, m) for(int c=0;c<(int)(m);c++)
-#define FORE(c, f, t) for(int c=(f);c<(t);c++)
+#define FORE(c, f, t) for(int c=(f);c<(int)(t);c++)
 #define LLI long long int
 #define MOD 1000000007
+
+
+char ccc = 0;
+int getInt() {
+    int r = 0;
+    while (!(ccc >= '0' && ccc <= '9')) ccc = getc_unlocked(stdin);
+    while (ccc >= '0' && ccc <= '9') {
+        r = r * 10 + (ccc - '0');
+        ccc = getc_unlocked(stdin);
+    }
+    return r;
+}
+
+int getString(char *start) {
+    char *data = start;
+    while (!(ccc > ' ' && ccc <= '~')) ccc = getc_unlocked(stdin);
+    while (ccc > ' ' && ccc <= '~') {
+        *data++ = ccc;
+        ccc = getc_unlocked(stdin);
+    }
+    *data = 0;
+    return data - start;
+}
+
+
+int getSignedInt() {
+    int r = 0;
+    while (!(ccc == '-' || (ccc >= '0' && ccc <= '9'))) ccc = getc_unlocked(stdin);
+    bool minus = ccc == '-';
+    if (minus) ccc = getc_unlocked(stdin);
+    while (ccc >= '0' && ccc <= '9') {
+        r = r * 10 + (ccc - '0');
+        ccc = getc_unlocked(stdin);
+    }
+    return minus ? -r : r;
+}
+
+long long int getLongLongInt() {
+    long long int r = 0;
+    while (!(ccc >= '0' && ccc <= '9')) ccc = getc_unlocked(stdin);
+    while (ccc >= '0' && ccc <= '9') {
+        r = r * 10 + (ccc - '0');
+        ccc = getc_unlocked(stdin);
+    }
+    return r;
+}
+
+
+template <class type> void print(type a) {
+    if (a < 0) {
+        putc_unlocked('-', stdout);
+        a = -a;
+    }
+    if (a == 0) {
+        putc_unlocked('0', stdout);
+    } else {
+        char result[20];
+        int resSize = 0;
+        while (a > 0) {
+            result[resSize++] = '0' + a % 10;
+            a /= 10;
+        }
+        while (--resSize >= 0) putc_unlocked(result[resSize], stdout);
+    }
+    putc_unlocked('\n', stdout);
+}
+
+void printString(const char *str) {
+    while (*str) putc_unlocked(*str++, stdout);
+    putc_unlocked('\n', stdout);
+}
 
 int power(int a, int b) {
     int sq = a;
@@ -24,16 +94,23 @@ int inv(int a) {
     return power(a, MOD - 2);
 }
 
+template <class type> type gcd(type a, type b) {
+    return b == 0 ? a : gcd(b, a % b);
+}
+
 void fail() {
     *((char *)0) = 0;
 }
 
+double cpu() {
+    return clock() / (double)CLOCKS_PER_SEC;
+}
 
 
 int main(void) {
-    int cases;
-    scanf("%d", &cases);
-    for(int cc=1;cc<=cases;cc++) {
-        printf("Case #%d:", cc);
+    int t = getInt();
+    FOR(tt, t) {
+        printf("Case #%d: ", tt + 1);
     }
 }
+
